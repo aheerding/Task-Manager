efd9881d21e9d8896cadac6d7caecb749cfd10cc
aheerding <aah3099@rit.edu>
2017-10-13 15:34:15 -0400

    Added calendar fragement
    Added calendar fragment to addNewTaskActivity allowing users to select a
    date from a popup calendar

---
+++ Task-Manager/Task_Manager/TaskManager/app/src/main/java/com/example/alex/taskmanager/AddNewTaskActivity.java
@@ -0,0 +1,62 @@
+package com.example.alex.taskmanager;
+
+import android.annotation.TargetApi;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.icu.util.Calendar;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+public class AddNewTaskActivity extends AppCompatActivity{
+
+    static EditText txtDate;
+
+    @TargetApi(21)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_task);
+        txtDate = (EditText) findViewById(R.id.in_date);
+        txtDate.setShowSoftInputOnFocus(false);
+
+        txtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTruitionDatePickerDialog(v);
+            }
+        });
+    }
+
+    //fragment to display the DatePicker
+    @TargetApi(24)
+    public static class DatePickerFragment extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener{
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            //get current date and set as default when the datepicker opens
+            final Calendar c = Calendar.getInstance();
+            int y = c.get(Calendar.YEAR);
+            int m = c.get(Calendar.MONTH);
+            int d = c.get(Calendar.DAY_OF_MONTH);
+
+            //create datepickerdialog and return it
+            return new DatePickerDialog(getActivity(), this, y, m, d);
+        }
+
+        public void onDateSet(DatePicker datePicker, int year, int month, int day){
+            //set the textview to the date selected
+            txtDate.setText(month + "/" + day +"/" + year);
+        }
+    }
+
+
+    public void showTruitionDatePickerDialog(View v){
+        DialogFragment dateFragment = new DatePickerFragment();
+        dateFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+}
